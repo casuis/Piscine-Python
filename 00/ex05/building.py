82ae5660c62a3174db6e1c7cb2be1364d0ed7fb5
Arthur Simon <rthrsmn@gmail.com>
2024-03-08 17:40:33 +0100

    clean code with flake8

--- 00/ex05/building.py
+++ 00/ex05/building.py
@@ -1,30 +1,31 @@
 import sys
 
-def main():
-	user_input = ""
-	buff = ",.!?:;()[]{}<>_-\"'`/\\@#$%^&*~+=|"
-	try:
-		if len(sys.argv) == 1:
-			user_input = input("Please provide a string: > ")
-			assert user_input
-		else:
-			user_input = sys.argv[1]
-	except EOFError:
-		return 
-	except:
-		raise AssertionError("TBD")
-	upper_case = sum(1 for c in user_input if c.isupper())
-	lower_case = sum(1 for c in user_input if c.islower())
-	space = sum(1 for c in user_input if c.isspace())
-	ponct = sum(1 for c in user_input if c in buff)
-	digit = sum(1 for c in user_input if c.isdigit())
 
-	print(upper_case, "upper letters")
-	print(lower_case, "lower letters")
-	print(ponct, "punctuation marks")
-	print(space, "spaces")
-	print(digit, "digits")
-	return
+def main():
+    user_input = ""
+    buff = ",.!?:;()[]{}<>_-\"'`/\\@#$%^&*~+=|"
+    try:
+        if len(sys.argv) == 1:
+            user_input = input("Please provide a string: > ")
+            assert user_input
+        else:
+            user_input = sys.argv[1]
+    except EOFError:
+        return
+    except Exception:
+        raise AssertionError("TBD")
+    upper_case = sum(1 for c in user_input if c.isupper())
+    lower_case = sum(1 for c in user_input if c.islower())
+    space = sum(1 for c in user_input if c.isspace())
+    ponct = sum(1 for c in user_input if c in buff)
+    digit = sum(1 for c in user_input if c.isdigit())
+    print(upper_case, "upper letters")
+    print(lower_case, "lower letters")
+    print(ponct, "punctuation marks")
+    print(space, "spaces")
+    print(digit, "digits")
+    return
+
 
 if __name__ == "__main__":
-	main()
+    main()
